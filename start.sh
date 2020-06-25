ebef668ec4a0cee29e620d27585d979588ca922b
DocW <docwhomc@gmail.com>
2020-06-24 22:41:38 -0400

    Upgrade to 1.16.1
    Upgrade to Spigot 1.16.1-R0.1-SNAPSHOT git-Spigot-758abbe-ab38754.

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-java -Xms1G -Xmx1G -XX:+UseConcMarkSweepGC -jar spigot-1.15.2.jar nogui
+java -Xms1G -Xmx1G -XX:+UseConcMarkSweepGC -jar spigot-1.16.1.jar nogui
 printf '\n'
 git status
